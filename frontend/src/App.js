fe69a0419dd1b9a0b147adde2870580da97182de
haris <harisiftikhar109@gmail.com>
2023-06-03 13:00:21 +0500

    '-'

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,29 @@
 import "./App.css";
+import NavbarBootstrap from "./components/NavbarBootstrap";
 import Sample from "./components/Sample";
+import React, { useState } from "react";
 
 function App() {
+  // hook for state management
+  const [mode, setMode] = useState("dark");
+
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "dark";
+    }
+  };
+
   return (
     <>
+      <NavbarBootstrap
+        title="EventSceduler"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
       <Sample />
     </>
   );
